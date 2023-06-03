1554758b7ed90fb45c18253323917fde211d329e
Enrico Massone <enrico.massone@hotmail.it>
2023-06-03 11:31:39 +0200

    fixed a few compiler warnings

--- src/Inventory.Domain/Framework/AggregateRoot.cs
+++ src/Inventory.Domain/Framework/AggregateRoot.cs
@@ -6,7 +6,9 @@ namespace Inventory.Domain.Framework
   {
     private readonly List<object> _changes;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     protected AggregateRoot()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
       this._changes = new List<object>();
       this.Version = -1;
