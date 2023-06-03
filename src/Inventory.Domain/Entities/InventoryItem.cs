9f44a821b5f04dcccc1c71b1653a65d3ca84bdeb
Enrico Massone <enrico.massone@hotmail.it>
2023-06-03 11:40:01 +0200

    defined a skeleton of the inventory item aggregate root

---
+++ src/Inventory.Domain/Entities/InventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Domain.Framework;
+using Inventory.Domain.ValueObjects;
+
+namespace Inventory.Domain.Entities
+{
+  public sealed class InventoryItem : AggregateRoot<InventoryItemId>
+  {
+    public bool IsActive { get; private set; }
+    public Quantity CurrentQuantity { get; private set; }
+    public Quantity MaximumAllowedQuantity { get; private set; }
+
+    protected override void ApplyToState(object @event)
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override void EnsureValidState()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
