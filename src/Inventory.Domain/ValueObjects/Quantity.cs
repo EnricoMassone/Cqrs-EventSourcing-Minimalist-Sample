27f4a669eddd3667db66007383e46559dbba526e
Enrico Massone <enrico.massone@hotmail.it>
2023-06-03 10:15:22 +0200

    wip: adding tests for Quantity value object

---
+++ src/Inventory.Domain/ValueObjects/Quantity.cs
@@ -0,0 +1,23 @@
+﻿namespace Inventory.Domain.ValueObjects
+{
+  public sealed record Quantity
+  {
+    public static Quantity FromInteger(int value)
+    {
+      if (value <= 0)
+      {
+        throw new ArgumentOutOfRangeException(
+          nameof(value),
+          $"{value} is not a valid quantity. Quantity must be positive integer number.");
+      }
+
+      return new Quantity(value);
+    }
+
+    public int Value { get; }
+
+    internal Quantity(int value) => this.Value = value;
+
+    public static implicit operator int(Quantity quantity) => quantity.Value;
+  }
+}
---
+++ test/Inventory.Domain.UnitTests/ValueObjects/QuantityTests.cs
@@ -0,0 +1,38 @@
+﻿using Inventory.Domain.ValueObjects;
+
+namespace Inventory.Domain.UnitTests.ValueObjects
+{
+  [TestFixture]
+  public sealed class QuantityTests
+  {
+    [Test]
+    public void FromInteger_Throws_ArgumentOutOfRangeException_When_Value_Is_Less_Than_Zero()
+    {
+      // ACT
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(
+        () => Quantity.FromInteger(-3)
+      );
+
+      // ASSERT
+      Assert.That(
+        exception.ParamName,
+        Is.EqualTo("value")
+      );
+
+      Assert.That(
+        exception.Message,
+        Contains.Substring("-3 is not a valid quantity. Quantity must be positive integer number.")
+      );
+    }
+
+    [Test]
+    public void FromInteger_Creates_New_Instance_Of_Quantity_From_Integer_Value()
+    {
+      // ACT
+      var result = Quantity.FromInteger(13);
+
+      // ASSERT
+      Assert.That(result.Value, Is.EqualTo(13));
+    }
+  }
+}
