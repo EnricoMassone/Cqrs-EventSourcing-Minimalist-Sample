c3bfb23accdb4500e57710ceaa6e0b88bd86f04a
Enrico Massone <enrico.massone@hotmail.it>
2023-06-04 18:34:15 +0200

    Completed unit tests for Deactivate

--- test/Inventory.Domain.UnitTests/Entities/InventoryItemTests_Deactivate.cs
+++ test/Inventory.Domain.UnitTests/Entities/InventoryItemTests_Deactivate.cs
@@ -26,5 +26,28 @@
 
       Assert.That(@event.Id, Is.EqualTo(target.Id.Value));
     }
+
+    [Test]
+    public void Deactivate_Does_Not_Raise_Event_When_Inventory_Item_Is_Not_Active()
+    {
+      // ARRANGE
+      var target = CreateTestTarget();
+
+      target.Deactivate();
+
+      target.ClearChanges();
+
+      // ACT
+      target.Deactivate();
+
+      // ASSERT
+      Assert.That(target.IsActive, Is.False);
+
+      // check events
+      var changes = target.GetChanges();
+
+      Assert.That(changes, Is.Not.Null);
+      Assert.That(changes, Is.Empty);
+    }
   }
 }
